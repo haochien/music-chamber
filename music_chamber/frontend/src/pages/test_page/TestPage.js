aa59aaabad8350cf59d112a46e5a069180a8ff5a
hao <haochien886@gmail.com>
2022-06-07 01:21:46 +0200

    test on player ui

--- music_chamber/frontend/src/pages/test_page/TestPage.js
+++ music_chamber/frontend/src/pages/test_page/TestPage.js
@@ -1,187 +1,202 @@
 import {useEffect, useState} from 'react';
-import MsgBar from '../../components/MsgBar';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import ListItemText from '@mui/material/ListItemText';
-import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Autocomplete from '@mui/material/Autocomplete';
-import Box from '@mui/material/Box';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import LinearProgress from '@mui/material/LinearProgress';
+import Collapse from '@mui/material/Collapse';
+import Box from '@mui/material/Box';
 
 
-const reachResult1 = [{'id': '0edJN9w4tY8VXhNPt5U5qB', 'title': 'tty', 'artist': 'artist1', 'image_url': 'https://i.scdn.co/image/ab67616d0000b2734c57b4acb4247e80a70ca388'},
-                      {'id': '3kIFJsq83ecH3ingfUXVMZ', 'title': 'ffb', 'artist': 'artist2', 'image_url': 'https://i.scdn.co/image/ab67616d0000b273ba02e4c2026b51da26aa58bb'},]
-const reachResult2 = [{'id': '49pMAJwZxDKFo4yquBV8wS', 'title': 'ttx', 'artist': 'artist3', 'image_url': 'https://i.scdn.co/image/ab67616d0000b273c96643131bb37cdf53890096'},
-                      {'id': '07NQhM4kudAA1CgC61sjfX', 'title': 'ffa', 'artist': 'artist4', 'image_url': 'https://i.scdn.co/image/ab67616d0000b27395a8175e345a0c14f3dddadd'},]
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+
+const featureBar = (featureValue, featureName) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', mb:1}}>
+      <Box sx={{ minWidth: 85 }}>
+        <Typography variant="body2" color="text.secondary">{featureName}</Typography>
+      </Box>
+      <Box sx={{ width: '100%', mr: 1, ml: 1 }}>
+        <LinearProgress variant="determinate" value={featureValue * 100} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(featureValue * 100)}%`}</Typography>
+      </Box>
+    </Box>
+  )
+}
+
+const featureTextLine = (label, body, footer) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', mb:1}}>
+      <Box>
+        <Typography variant="body2" color="text.secondary" align='left'>
+          {label}
+        </Typography>
+      </Box>
+      <Box sx={{ mr: 1, ml: 1 }}>
+        <Typography variant="body2" color="text.secondary" align='left'>
+          {body}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography variant="body2" color="primary.main" align='left' sx={{fontWeight: 'bold'}}>
+          {footer}
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
+
+const translatePopularity = (popularityValue) => {
+  if (popularityValue >= 80) {return 'Extremely Popular'} 
+  if (popularityValue >= 60 && popularityValue < 80) {return 'Very Popular'} 
+  if (popularityValue >= 45 && popularityValue < 60) {return 'In Vogue'} 
+  if (popularityValue >= 30 && popularityValue < 45) {return 'Unpopular'} 
+  if (popularityValue < 30) {return 'Rarely Known'} 
+}
+
 
 export default function TestPage() {
-    const [open, setOpen] = useState(true);
-    const [openSongSearchList, setOpenSongSearchList] = useState(false);
-    const [queryResult, setQueryResult] = useState([])
+  const durationInMs = 180000
+  const song_energy = 0.842
+  const song_danceability = 0.842
+  const song_happiness = 0.842
+  const song_acousticness = 0.142
+  const song_speechiness = 0.0556
+  const song_popularity = 72
+  const song_tempo = 118.211
+  const [isPlay, setIsPlay] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
 
-    const [queryInput, setQueryInput] = useState([])
-    const [songList, setSongList] = useState([])
+  const [progress, setProgress] = useState(0);
+  const [expanded, setExpanded] = useState(false);
 
-    const [showMsg, setShowMsg] = useState(false);
-    const [err, setErr] = useState('')
 
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
+  const togglePlay = () => {
+    if (isPlay) {
+      setIsPlay(false)
+    } else {
+      setIsPlay(true)
+    }
+  }
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      setIsFavorite(false)
+    } else {
+      setIsFavorite(true)
+    }
+  }
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return Math.min((oldProgress * durationInMs  + 1000)/durationInMs, 1);
+      });
+    }, 1000);
   
-    const handleClose = (event, reason) => {
-      if (reason && reason == "backdropClick") 
-          return;
-      setOpen(false);
-    }
-
-    const deleteListItem = (index) => {
-      setSongList(preSonglist => {
-        //return preSonglist.filter(song => id !== song.id)
-        return [...preSonglist.slice(0, index), ...preSonglist.slice(index + 1, preSonglist.length)]
-      })
-    }
-
-    const addToPendingList = (item) => {
-      setSongList(preSonglist => {
-        return [...preSonglist, item]
-      })
-    }
-
-    const confirmAddSong = (event, reason) => {
-      if (songList.length > 0) {
-         let arraySongId = songList.map(songObj => songObj.id)
-         console.log(arraySongId.join(","))
-         handleClose(event, reason)
-      } else {
-        setErr('Please add at least one song')
-        switchMsgBar(true)
-      }
-    }
-
-    const switchMsgBar = (trueOrFalse) => {
-      setShowMsg(trueOrFalse)
+    return () => {
+      clearInterval(timer);
     };
+  }, [durationInMs]);
 
-    useEffect(() => {
-      //TODO: put spotify search query api here
-      if (queryInput=='t') {
-        setQueryResult(reachResult1)
-      } else if (queryInput=='f') {
-        setQueryResult(reachResult2)
-      } else {
-        setQueryResult([])
-      }
-    }, [queryInput])
-  
-    return (
-      <div>
+  return (
+    <Card sx={{ maxWidth: 350, minHeight: 500, borderRadius:3}}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="300"
+        image="https://i.scdn.co/image/ab67616d0000b273ba02e4c2026b51da26aa58bb"
+        sx={{p:2, borderRadius:8}}
+      />
+      
+      <CardContent>
+        <Typography variant="h3" color="text.secondary" component="div" align='center' sx={{fontSize: 15, mb: 3}}>
+          New West
+        </Typography>
+        <Typography variant="h3" color="primary.main" align='center' sx={{fontSize: 35, mb: 4}}>
+          Those Eyes
+        </Typography>
+        <LinearProgress variant="determinate" value={progress * 100} sx={{ml: 1, mr: 1, mb:2}}/>
+      </CardContent>
 
-        <Dialog open={open} onClose={handleClose} scroll='paper' fullWidth={true} disableEscapeKeyDown={true}>
-          <DialogTitle>Add Songs to Playlist</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Search and add songs to the playlist of your chamber.
-            </DialogContentText>
+      <CardActions disableSpacing>
+        <IconButton aria-label="play or mute" onClick={togglePlay}>
+          {isPlay ? <VolumeOffIcon /> : <PlayCircleOutlineIcon />}
+        </IconButton>
+        <IconButton aria-label="add to favorites" onClick={toggleFavorite}>
+          {isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+        </IconButton>
 
-            <Autocomplete
-                sx={{ mt: 3, mb: 1 }}
-                freeSolo
-                
-                open={openSongSearchList}
-                onClose={() => setOpenSongSearchList(false)}
-                onChange={(event, newValue) => {
-                  console.log('song selected: ', newValue)
-                  if (newValue != null && newValue.hasOwnProperty('title')) {
-                    addToPendingList(newValue)
-                  }
-                  
-                }}
-                //inputValue={queryInput}
-                onInputChange={(event, newInputValue) => {
-                  if (newInputValue.length === 0) {
-                    if (openSongSearchList) setOpenSongSearchList(false);
-                  } else {
-                    if (!openSongSearchList) setOpenSongSearchList(true);
-                  }
-                  setQueryInput(newInputValue);
-                }}
-                id="song-query"
-                options={queryResult}
-                filterOptions={(options, state) => options}
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
 
-                getOptionLabel={(option) => {
-                  //return option.title || ""
-                  if (option.hasOwnProperty('title')) {
-                    return option.title;
-                  }
-                  return option;
-                }}
-                renderOption={(props, option) => (
-                  <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-                    <img
-                      loading="lazy" width="50" src={option.image_url} alt=""
-                    />
-                    {option.title} ({option.artist})
-                  </Box>
-                )}
+      
 
-                renderInput={(params) => {
-                  return <TextField {...params} autoFocus label="Search Song" />
-                }}
-              />
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography variant="h5" color="text.secondary" align='left' sx={{fontSize: 15, mb: 2, mt: 2, fontWeight:'bold'}}>
+            Song Feature
+          </Typography>
+          {featureBar(song_energy, 'Energy')}
+          {featureBar(song_danceability, 'Danceability')}
+          {featureBar(song_happiness, 'Happiness')}
+          {featureBar(song_acousticness, 'Acousticness')}
+          {featureBar(song_speechiness, 'Speechiness')}
 
-              <List>
-                {songList.length>0 && songList.map((event, index) => {
-                  const listItemId = `list-item-${index}`;
-                  const labelId = `list-content-${index}`;
-                  const dividerId = `list-divider-${index}`;
-                  console.log('currrent song list: ', songList)
+          {featureTextLine('Tempo:', `${song_tempo} (BPM)`, '')}
+          {featureTextLine('Popularity:', `${song_popularity} / 100 points`, `(${translatePopularity(song_popularity)})`)}
+        </CardContent>
+      </Collapse>
 
-                  return(
-                    <div key={index}>
-                      {index>0 ? <Divider key={dividerId}/> : null}
-                      
-                      <ListItem 
-                        key={listItemId}
-                        button
-                        secondaryAction={
-                          <IconButton edge="end" aria-label="delete" size="large" onClick={() => deleteListItem(index)}>
-                            <DeleteForeverIcon fontSize="medium" sx={{ color: 'primary.main'}} />
-                          </IconButton>
-                        }
-                      >
-                        <ListItemAvatar>
-                          <Avatar alt="" src={event.image_url} />
-                        </ListItemAvatar>
-                        <ListItemText id={labelId} primary={event.title} secondary={event.artist} />
-                      </ListItem>
-                      
-                    </div>
-                  );
-                })}
-                
-              </List>
-
-              
-
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={confirmAddSong}>Add Song</Button>
-          </DialogActions>
-        </Dialog>
-
-        <MsgBar showMsg={showMsg} msgType="error" duration={3000} msgContent={err} switchMsgBar={switchMsgBar} />
-      </div>
-    );
+    </Card>
+  );
 }
+
+
+
+
+// const song_energy = 0.842
+// const song_danceability = 0.842
+// const song_happiness = 0.842
+// const song_acousticness = 0.142
+// const song_speechiness = 0.0556
+// const song_popularity = 72
+// const song_tempo = 118.211
